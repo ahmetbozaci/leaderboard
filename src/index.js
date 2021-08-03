77386ca1e753f1698bbab818d1ef07e85d6999a9
ahmetbozaci <ahmt9417@gmail.com>
2021-08-03 16:21:52 +0300

    Add basic function for display list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './scss/style.scss';
+
+const scores = [
+  { name: 'name1', score: 100 },
+  { name: 'name2', score: 200 },
+  { name: 'name3', score: 300 },
+];
+const leaderBoard = document.getElementsByClassName('leader-board');
+
+const displayBoard = () => {
+  scores.forEach((score) => {
+    const li = document.createElement('li')
+    leaderBoard[0].appendChild(li);
+    li.append(`${score.name} ---- ${score.score}`)
+  });
+};
+displayBoard();
